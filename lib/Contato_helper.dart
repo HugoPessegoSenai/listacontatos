61a3830b6933636caa4ed12a5aa45cde71195cb0
Hugo Christian Senai <hugo.pessego@to.docente.senai.br>
2021-11-09 21:35:57 -0300

    Novo commit

--- lib/Contato_helper.dart
+++ lib/Contato_helper.dart
@@ -2,47 +2,95 @@ import 'dart:async';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
+class ContatoHelper {
+  static ContatoHelper _databaseHelper = new ContatoHelper();
+  static Database _database = new Database();
+
 //Atributos da tabela e banco de dados (Nome da tabela e coluna)
 
-final String contatoTable = "contatoTable";
-final String idColumn = "idColumn";
-final String nomeColumn = "nomeColumn";
-final String emailColumn = "emailColumn";
-final String telefoneColumn = "telefoneColumn";
-final String imagemColumn = "imagemColumn";
+  final String contatoTable = "contatoTable";
+  final String idColumn = "idColumn";
+  final String nomeColumn = "nomeColumn";
+  final String emailColumn = "emailColumn";
+  final String telefoneColumn = "telefoneColumn";
+  final String imagemColumn = "imagemColumn";
 
-class ContatoHelper {
-  static final ContatoHelper _instance = ContatoHelper.internal();
+  ContatoHelper._createInstance();
 
-  factory ContatoHelper() => _instance;
-
-  Database _db;
-
-  Future<Database> get db async {
-    if (_db != null) {
-      return _db;
-    } else {
-      _db = await initDb();
-      return _db;
+  factory ContatoHelper() {
+    if (_databaseHelper == null) {
+      _databaseHelper = ContatoHelper._createInstance();
     }
+
+    return _databaseHelper;
   }
 
-  Future<Database> initDb() async {
+  Future<Database> get database async {
+    if (_database == null) {
+      _database = await inicializaBanco();
+    }
+
+    return _database;
+  }
+
+  Future<Database> inicializaBanco() async {
     final databasePath = await getDatabasesPath();
-    final path = join(databasePath, "contato.db");
+    final path = join(databasePath, 'contatos.db');
 
-    return await openDatabase(path, version: 1,
-        onCreate: (Database db, int newversion) async {
-      await db.execute(
-          "CREATE TABLE $contatoTable($idColumn INTEGER PRIMARY KEY, $nomeColumn TEXT," +
-              " $emailColumn TEXT, $telefoneColumn TEXT, $imagemColumn TEXT)");
-    });
+    var bandoDeContatos =
+        await openDatabase(path, version: 1, onCreate: _criaBanco);
+    return bandoDeContatos;
   }
 
-  Future<Contato> saveContato(Contato contato)async{
-    Database dbContato = await db;
-    contato.id = await dbContato.insert(contatoTable, contato.toMap());
-    return contato;
+  void _criaBanco(Database db, int versao) async {
+    await db.execute(
+        "CREATE TABLE $contatoTable($idColumn INTEGER PRIMARY KEY, $nomeColumn TEXT," +
+            " $emailColumn TEXT, $telefoneColumn TEXT, $imagemColumn TEXT)");
+
+/*
+De uma forma direta, esse seria o SQL
+CREATE TABLE tabela_contato(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+nome TEXT,
+email TEXT
+);
+*/
+  }
+
+  Future<List<Map<String, dynamic>>> getContatoMapList() async {
+    Database db = await this.database;
+    var result = await db.rawQuery("SELECT * FROM tabela_contato");
+    return result;
+  }
+
+  Future<int> inserirContato(Contato contato) async {
+    Database db = await this.database;
+    var result = await db.insert(contatoTable, contato.toMap());
+    return result;
+  }
+
+  Future<int> atualizarContato(Contato contato, int id) async {
+    var db = await this.database;
+    var result = await db.rawUpdate(
+        "UPDATE $contatoTable SET $nomeColumn = '${contato.nome}', $emailColumn = '${contato.email}', $telefoneColumn = '${contato.telefone}', $imagemColumn = '${contato.imagem}'   WHERE $idColumn = '$id'");
+    return result;
+  }
+
+  Future<int> apagarContato(int id) async {
+    var db = await this.database;
+    int result =
+        await db.rawDelete('DELETE FROM $contatoTable WHERE $idColumn = $id');
+    return result;
+  }
+
+  Future<List<Contato>> getListaDeContato() async {
+    var contatoMapList = await getContatoMapList();
+    int count = contatoMapList.length;
+    List<Contato> listaDeContatos = List.generate<Contato>();
+    for (int i = 0; i < count; i++) {
+      listaDeContatos.add(Contato.fromMapObject(contatoMapList[i]));
+    }
+    return listaDeContatos;
   }
 }
 
@@ -53,21 +101,30 @@ class Contato {
   String telefone;
   String imagem;
 
-  Contato();
+  Contato(this.id,this.nome, this.email, this.telefone, this.imagem);
 
-  Map toMap(){
-    Map<String,dynamic> map = {
-      nomeColumn: nome,
-      emailColumn: email,
-      telefoneColumn: telefone,
-      imagemColumn: imagem
-    };
-
-    if(id!=null){
-      map[idColumn] = id;
+  Map<String, dynamic> toMap() {
+    var map = Map<String, dynamic>();
+    if (id != null) {
+      map['id'] = id;
     }
+    map['nome'] = nome;
+    map['email'] = email;
+    map['telefone'] = telefone;
+    map['imagem'] = imagem;
     return map;
   }
-  
-}
 
+  Contato.fromMapObject(Map<String, dynamic> map) {
+    this.id = map['id'];
+    this.nome = map['nome'];
+    this.email = map['email'];
+    this.telefone = map['telefone'];
+    this.imagem = map['imagem'];
+  }
+
+  @override
+  String toString() {
+    return "Contact(id: $id, name: $nome, email: $email, phone: $telefone, img: $imagem)";
+  }
+}
